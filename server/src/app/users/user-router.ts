731d600482aa047777dfe7c0116df4f3008df190
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-10 17:26:34 +0100

    added login route

--- server/src/app/users/user-router.ts
+++ server/src/app/users/user-router.ts
@@ -1,5 +1,6 @@
-import { tRouter, tProcedure } from "../../config/trpc";
+import { tRouter, tProcedure, tError } from "../../config/trpc";
 import z from "zod";
+import { signUp, login } from "./controllers/exports";
 
 /** endpoints for user-related operations */
 const userRouter = tRouter({
@@ -8,10 +9,79 @@ const userRouter = tRouter({
       z.object({
         username: z.string(),
         email: z.string().email(),
-        password: z.string().min(8).max(64) /* for opusbopus */,
+        password: z.string().min(8).max(64) /* for opusbopus :) */,
       })
     )
-    .query(async ({ input }) => {}),
+    .mutation(async ({ input }) => {
+      const res = await signUp(input);
+      switch (res.ok) {
+        case true: {
+          return { ok: true, token: res.token } as const;
+        }
+        case false: {
+          switch (res.message) {
+            case "an error occured":
+            case "could not generate token": {
+              throw new tError({
+                message: res.message,
+                code: "INTERNAL_SERVER_ERROR",
+              });
+            }
+            case "user already exists": {
+              throw new tError({
+                message: "could not create user",
+                code: "FORBIDDEN",
+                cause: res.message,
+              });
+            }
+          }
+        }
+        default: {
+          return { ok: false, message: "something unexpected happened" };
+        }
+      }
+    }),
+  login: tProcedure
+    .input(
+      z.object({
+        userData: z.object({
+          email: z.string().email(),
+          password: z.string().min(8).max(64),
+        }),
+        options: z.object({
+          expires: z.union([z.literal("short"), z.literal("long")]),
+        }),
+      })
+    )
+    .mutation(async ({ input }) => {
+      const res = await login(input.userData, input.options);
+
+      switch (res.ok) {
+        case true: {
+          return { ok: res.ok, token: res.token } as const;
+        }
+        case false: {
+          switch (res.message) {
+            case "an error occured":
+            case "could not generate token": {
+              throw new tError({
+                message: res.message,
+                code: "INTERNAL_SERVER_ERROR",
+              });
+            }
+            case "user not found": {
+              throw new tError({ message: "not found", code: "NOT_FOUND" });
+            }
+            case "wrong password": {
+              throw new tError({
+                message: "invalid login details",
+                code: "UNAUTHORIZED",
+              });
+            }
+          }
+        }
+      }
+    }),
 });
 
 export default userRouter;
