a364f48cc2d1e28d51f4196904887125d867eb79
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-10 21:50:47 +0100

    type changes

--- server/src/app/auth/token.ts
+++ server/src/app/auth/token.ts
@@ -1,21 +1,28 @@
 import jwt from "jsonwebtoken";
 import { env } from "../..";
 
+type Payload = {
+  iat: number;
+  data: string;
+};
+
 const token = {
   /** Generate new jwt token */
-  async generate(payload: string, options?: { expires: "short" | "long" }) {
+  async generate(
+    payloadInput: string,
+    options?: { expires: "short" | "long" }
+  ) {
     try {
       if (!env.jwtSecret) {
         console.error("jwt secret missing");
         return { ok: false, message: "jwt secret is missing" } as const;
       }
-      /** generated token from @param payload and `jwt secret` */
-      const token = jwt.sign(
-        { iat: Date.now(), payload },
-        env.jwtSecret,
-        /* short logins expire after 1 day */
-        { expiresIn: options?.expires === "long" ? "30d" : "1d" }
-      );
+      const payload = { iat: Date.now(), data: payloadInput };
+      /* short logins expire after 1 day */
+      const expiresIn = options?.expires === "long" ? "30d" : "1d";
+
+      /** generated token from @param payloadInput and `jwt secret` */
+      const token = jwt.sign(payload, env.jwtSecret, { expiresIn });
       return { ok: true, token } as const;
     } catch (error) {
       console.error(error);
@@ -31,11 +38,8 @@ const token = {
         return { ok: false, message: "jwt secret is missing" } as const;
       }
       // verify the token and return the payload
-      const isValidToken = jwt.verify(token, env.jwtSecret) as {
-        iat: number;
-        payload: string;
-      };
-      return { ok: true, token: isValidToken.payload } as const;
+      const isValidToken = jwt.verify(token, env.jwtSecret) as Payload;
+      return { ok: true, token: isValidToken.data } as const;
     } catch (error) {
       if (error instanceof jwt.TokenExpiredError) {
         return { ok: false, message: "token expired" } as const;
@@ -48,8 +52,10 @@ const token = {
   /** Decode jwt token */
   async decode(token: string) {
     try {
-      const { payload } = jwt.decode(token) as { iat: number; payload: string };
-      return { ok: true, data: payload } as const;
+      /** Data stored in the jwt token */
+      const payload = jwt.decode(token) as Payload;
+      const { data } = payload;
+      return { ok: true, data } as const;
     } catch (error) {
       console.error(error);
       return { ok: false, message: "an error occured" } as const;
