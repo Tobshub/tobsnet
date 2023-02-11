bfd6f41ed6ddfa0740a1ea457cdd32336d80051f
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-11 02:24:43 +0100

    load feed router

--- server/src/app/posts/post-router.ts
+++ server/src/app/posts/post-router.ts
@@ -1,5 +1,5 @@
 import { z } from "zod";
-import { tProcedure, tRouter } from "../../config";
+import { tError, tProcedure, tRouter } from "../../config";
 import { loadFeed } from "./controllers";
 
 const postRouter = tRouter({
@@ -9,9 +9,42 @@ const postRouter = tRouter({
       const { token } = ctx.auth;
       if (!token) {
         // load random feed
-        return;
+        throw new tError({ message: "no user token", code: "UNAUTHORIZED" });
       }
-      const feed = await loadFeed({ token, size: 20, cursor: input.cursor });
+      const res = await loadFeed({ token, size: 20, cursor: input.cursor });
+
+      switch (res.ok) {
+        case true: {
+          return { ok: res.ok, data: res.data };
+        }
+        case false: {
+          switch (res.message) {
+            case "an error occured":
+            case "failed to generate feed": {
+              throw new tError({
+                message: "an error occured",
+                code: "INTERNAL_SERVER_ERROR",
+                cause: res.message,
+              });
+            }
+            case "user token is missing": {
+              throw new tError({
+                message: "please sign up or login",
+                code: "UNAUTHORIZED",
+                cause: res.message,
+              });
+            }
+          }
+        }
+        default: {
+          console.error("something unexpected happened");
+          throw new tError({
+            message: "something unexpected happened",
+            code: "BAD_REQUEST",
+            cause: "unknown",
+          });
+        }
+      }
     }),
 });
 
