627f76f3ef5cf250144cb411bfaeb1852bef70ee
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-15 20:08:21 +0100

    replaced console.log with `LOG` fuction

--- server/src/config/prisma.ts
+++ server/src/config/prisma.ts
@@ -1,21 +1,19 @@
 import { PrismaClient, Prisma } from "@prisma/client";
+import { LOG } from "../functions";
 
 /** Instance of the prisma client */
 export const usePrisma = new PrismaClient();
 
 /** Extract types from prisma queries */
-export type PrismaType<T extends (...args: any) => Promise<any>> =
-  Prisma.PromiseReturnType<T>;
+export type PrismaType<T extends (...args: any) => Promise<any>> = Prisma.PromiseReturnType<T>;
 
-/**
- * Connect to the mongo cluster at the uri in `.env` with prisma
- */
+/** Connect to the mongo cluster at the uri in `.env` with prisma */
 export async function PrismaConnect() {
   try {
     await usePrisma.$connect().then(() => {
-      console.log("connected to db");
+      LOG.info("Prisma: connected to db");
     });
   } catch (error) {
-    console.error(error);
+    LOG.error(error, "could not connect to prisma");
   }
 }
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,13 +3,14 @@ import cors from "cors";
 import { config } from "dotenv";
 import { PrismaConnect } from "./config";
 import trpcRouter from "./app/router";
+import { LOG } from "./functions";
 
 config();
 PrismaConnect();
 
 /** Exports global enviroment variables from `.env` */
 export const env = {
-  port: process.env.PORT,
+  port: process.env.PORT || 8080,
   jwtSecret: process.env.JWT_SECRET,
 };
 
@@ -19,10 +20,16 @@ app.use(cors(), express.json());
 // "/api" endpoints use trpcRouter
 app.use("/api", trpcRouter);
 
+app.use("/health", (req, res) => {
+  LOG.info("health check... done");
+  res.send({ status: "alive and well" });
+});
+
 app.use("/", (req, res) => {
   res.send({ message: "hello world" });
 });
 
 app.listen(env.port, () => {
-  console.log("live on port:", env.port);
+  console.log("app live");
+  LOG.info(`live port(${env.port})`);
 });
