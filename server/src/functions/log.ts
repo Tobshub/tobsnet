f6ce92ac5eb9745f760977b2a022c257e0276cd9
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-15 20:07:43 +0100

    configured logging

--- server/src/functions/log.ts
+++ server/src/functions/log.ts
@@ -1,6 +1,6 @@
 import pino from "pino";
 
-const pinoLog = pino({ transport: { target: "pino-pretty" } });
+const pinoLog = pino(pino.destination(`${process.cwd()}/dev.log`));
 
 /** Pino Log Wrapper */
 export const LOG = pinoLog;
