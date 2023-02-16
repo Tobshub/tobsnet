db94db4df691f7173ff9193f8cbd0deb557f69a7
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-16 05:54:39 +0100

    added comments route

--- server/src/app/router.ts
+++ server/src/app/router.ts
@@ -2,10 +2,12 @@ import * as trpcExpress from "@trpc/server/adapters/express";
 import { createContext, tRouter } from "../config";
 import userRouter from "./users/user-router";
 import postRouter from "./posts/post-router";
+import commentRouter from "./comments/comment-router";
 
 const appRouter = tRouter({
   users: userRouter,
   posts: postRouter,
+  comments: commentRouter,
 });
 
 export default trpcExpress.createExpressMiddleware({
