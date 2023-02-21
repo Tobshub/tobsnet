49f6928ab0f6aaa390a6194010450f6a686c68aa
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-21 08:01:54 +0100

    import `httpBatchLink` from the right source

--- client/src/context/trpc.ts
+++ client/src/context/trpc.ts
@@ -1,13 +1,15 @@
 import token from "@services/token";
 import { QueryClient } from "@tanstack/react-query";
-import { httpBatchLink } from "@trpc/client";
+import { httpBatchLink } from "@trpc/react-query";
 import trpc from "@utils/trpc";
 
 // change url for production deploy
 const { PROD } = import.meta.env;
 const API_URL = PROD ? "" : "http://localhost:8080/api";
 
-export const appQueryClient = new QueryClient();
+export const appQueryClient = new QueryClient({
+  defaultOptions: { queries: { staleTime: 1000 * 60, refetchOnWindowFocus: false } },
+});
 
 export const trpcClient = trpc.createClient({
   links: [httpBatchLink({ url: API_URL, headers: () => ({ authorization: token.get() }) })],
