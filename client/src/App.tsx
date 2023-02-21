564bf51c83995276d4368850aadc63afa092353c
Tobani Esan-George <tobanigeorge@gmail.com>
2023-02-21 08:02:30 +0100

    routes config

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,6 @@
 import { appQueryClient, trpcClient } from "@context/trpc";
+import LoginPage from "@pages/Auth/login";
+import SignUpPage from "@pages/Auth/signUp";
 import { QueryClientProvider } from "@tanstack/react-query";
 import trpc from "@utils/trpc";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
@@ -7,6 +9,19 @@ const router = createBrowserRouter([
   {
     index: true,
     element: <>Hello world </>,
+  },
+  {
+    path: "/auth",
+    children: [
+      {
+        path: "login",
+        element: <LoginPage />,
+      },
+      {
+        path: "sign-up",
+        element: <SignUpPage />,
+      },
+    ],
   },
 ]);
 
